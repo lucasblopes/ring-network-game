f3d4add4c30c0aa6deca61a42f4fdefb4e7e6659
Lucas Lopes <Lucas.Lopes@landisgyr.com>
2024-08-09 11:20:05 -0300

    dinf ips

--- settings.py
+++ settings.py
@@ -9,7 +9,13 @@ NUM_PLAYERS = 4
 
 # Network config
 BUFFER_SIZE = 1024
-HOSTS = ["127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1"]  # IPs dos jogadores
+# HOSTS = ["127.0.0.1", "127.0.0.1", "127.0.0.1", "127.0.0.1"]  # IPs dos jogadores
+HOSTS = [
+    "200.17.202.34",  # cpu1
+    "200.17.202.35",  # cpu2
+    "200.17.202.98",  # zara
+    "200.17.202.7",  # orval
+]
 PORTS = [5000, 5001, 5002, 5004]  # Portas dos jogadores
 
 # Misc
